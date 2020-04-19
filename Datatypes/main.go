fe7fadba5e59ca0e8ad5b50c6ead3f0a3f6fcbd1
Anmol Gupta <anmol.gupta.che17@itbhu.ac.in>
2020-04-19 14:28:34 +0530

    Add twitterhandler

--- Datatypes/main.go
+++ Datatypes/main.go
@@ -1,9 +1,15 @@
 package main
 
 import "godatatypes/organization"
+import "fmt"
 
 func main() {
 	p := organization.NewPerson( firstname: "Anmol", lastname: "Gupta")
+	err := p.SetTwitterhandler(handler: "@anmol_gupta")
+	if err != nil {
+		fmt.Printf(format: "An error occurred setting twitter handler: %s\n", err.Error())
+	}
+	println(p.TwitterHandler())
 	println(p.ID())
 	println(p.fullname())
 }
--- Datatypes/organization/person.go
+++ Datatypes/organization/person.go
@@ -9,6 +9,7 @@ type Indentifiable interface {
 type Person struct {
 	firstname string
 	lastname  string
+	twitterhandler string
 }
 
 func NewPerson(firstname, lastname string) Person {
@@ -18,10 +19,25 @@ func NewPerson(firstname, lastname string) Person {
 	}
 }
 
-func (p Person) fullname() string {
+func (p *Person) fullname() string {
 	return fmt.Sprintf("%s %s", p.firstname, p.lastname)
 }
 
-func (p Person) ID() string {
+func (p *Person) ID() string {
 	return "12345"
+}
+
+func (p *Person) SetTwitterHandler(handler string) error {
+	if len(handler) == 0 {
+		p.twitterhandler = handler
+	} else if !strings.HasPrefix(handler, prefix: "@") {
+		return errors.New("twitter handle must start with an @ symbol")
+	}
+
+	p.twitterhandler = handler
+	return nil
+}
+
+func (p Person) TwitterHandler() string {
+	return p.twitterhandler
 }
