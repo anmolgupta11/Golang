f6234f679a83988294f3911e8200fb6cb37f25c2
Anmol Gupta <anmol.gupta.che17@itbhu.ac.in>
2020-05-03 22:58:34 +0530

    Add services

---
+++ Go_Project_si2020/services/auth_service.go
@@ -0,0 +1,54 @@
+package services
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"Go_Project_si2020/api/parameters"
+	"Go_Project_si2020/core/authentication"
+	"Go_Project_si2020/services/models"
+
+	jwt "github.com/dgrijalva/jwt-go"
+	request "github.com/dgrijalva/jwt-go/request"
+)
+
+func Login(requestUser *models.User) (int, []byte) {
+	authBackend := authentication.InitJWTAuthenticationBackend()
+
+	if authBackend.Authenticate(requestUser) {
+		token, err := authBackend.GenerateToken(requestUser.UUID)
+		if err != nil {
+			return http.StatusInternalServerError, []byte("")
+		} else {
+			response, _ := json.Marshal(parameters.TokenAuthentication{token})
+			return http.StatusOK, response
+		}
+	}
+
+	return http.StatusUnauthorized, []byte("")
+}
+
+func RefreshToken(requestUser *models.User) []byte {
+	authBackend := authentication.InitJWTAuthenticationBackend()
+	token, err := authBackend.GenerateToken(requestUser.UUID)
+	if err != nil {
+		panic(err)
+	}
+	response, err := json.Marshal(parameters.TokenAuthentication{token})
+	if err != nil {
+		panic(err)
+	}
+	return response
+}
+
+func Logout(req *http.Request) error {
+	authBackend := authentication.InitJWTAuthenticationBackend()
+	tokenRequest, err := request.ParseFromRequest(req, request.OAuth2Extractor, func(token *jwt.Token) (interface{}, error) {
+		return authBackend.PublicKey, nil
+	})
+	if err != nil {
+		return err
+	}
+	tokenString := req.Header.Get("Authorization")
+	return authBackend.Logout(tokenString, tokenRequest)
+}
